40f7397ad73c83d0b3b6b70f72a97ef027de74ba
Bjørn Mork <bjorn@mork.no>
2021-03-10 11:01:37 +0100

    realtek: kernel API changes for v5.10
    Signed-off-by: Bjørn Mork <bjorn@mork.no>

--- target/linux/realtek/files-5.10/drivers/net/dsa/rtl83xx/common.c
+++ target/linux/realtek/files-5.10/drivers/net/dsa/rtl83xx/common.c
@@ -18,27 +18,6 @@ extern const struct dsa_switch_ops rtl930x_switch_ops;
 
 DEFINE_MUTEX(smi_lock);
 
-// TODO: unused
-static void dump_fdb(struct rtl838x_switch_priv *priv)
-{
-	struct rtl838x_l2_entry e;
-	int i;
-
-	mutex_lock(&priv->reg_mutex);
-
-	for (i = 0; i < priv->fib_entries; i++) {
-		priv->r->read_l2_entry_using_hash(i >> 2, i & 0x3, &e);
-
-		if (!e.valid) /* Check for invalid entry */
-			continue;
-
-		pr_debug("-> port %02d: mac %pM, vid: %d, rvid: %d, MC: %d, %d\n",
-			e.port, &e.mac[0], e.vid, e.rvid, e.is_ip_mc, e.is_ipv6_mc);
-	}
-
-	mutex_unlock(&priv->reg_mutex);
-}
-
 int rtl83xx_port_get_stp_state(struct rtl838x_switch_priv *priv, int port)
 {
 	u32 msti = 0;
@@ -570,8 +549,7 @@ static int __init rtl83xx_sw_probe(struct platform_device *pdev)
 	if (!priv)
 		return -ENOMEM;
 
-	priv->ds = dsa_switch_alloc(dev, DSA_MAX_PORTS);
-
+	priv->ds = devm_kzalloc(dev, sizeof(*priv->ds), GFP_KERNEL);
 	if (!priv->ds)
 		return -ENOMEM;
 	priv->ds->dev = dev;
--- target/linux/realtek/files-5.10/drivers/net/dsa/rtl83xx/dsa.c
+++ target/linux/realtek/files-5.10/drivers/net/dsa/rtl83xx/dsa.c
@@ -145,7 +145,7 @@ const struct rtl83xx_mib_desc rtl83xx_mib[] = {
 /* DSA callbacks */
 
 
-static enum dsa_tag_protocol rtl83xx_get_tag_protocol(struct dsa_switch *ds, int port)
+static enum dsa_tag_protocol rtl83xx_get_tag_protocol(struct dsa_switch *ds, int port, enum dsa_tag_protocol mp)
 {
 	/* The switch does not tag the frames, instead internally the header
 	 * structure for each packet is tagged accordingly.
@@ -474,9 +474,11 @@ static void rtl83xx_phylink_mac_link_down(struct dsa_switch *ds, int port,
 }
 
 static void rtl83xx_phylink_mac_link_up(struct dsa_switch *ds, int port,
-				   unsigned int mode,
-				   phy_interface_t interface,
-				   struct phy_device *phydev)
+					unsigned int mode,
+					phy_interface_t interface,
+					struct phy_device *phydev,
+					int speed, int duplex,
+					bool tx_pause, bool rx_pause)
 {
 	struct rtl838x_switch_priv *priv = ds->priv;
 	/* Restart TX/RX to port */
@@ -826,11 +828,15 @@ void rtl930x_fast_age(struct dsa_switch *ds, int port)
 }
 
 static int rtl83xx_vlan_filtering(struct dsa_switch *ds, int port,
-				  bool vlan_filtering)
+				  bool vlan_filtering,
+				  struct switchdev_trans *trans)
 {
 	struct rtl838x_switch_priv *priv = ds->priv;
 
 	pr_debug("%s: port %d\n", __func__, port);
+	if (switchdev_trans_ph_prepare(trans))
+		return 0;
+
 	mutex_lock(&priv->reg_mutex);
 
 	if (vlan_filtering) {
